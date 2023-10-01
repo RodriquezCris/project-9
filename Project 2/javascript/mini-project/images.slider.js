d7edab84b8fe47d7e691724cc6e99ce3f7296de4
RodriquezCris <142678152+RodriquezCris@users.noreply.github.com>
2023-09-30 21:28:24 -0500

    initial commit

---
+++ Project 2/javascript/mini-project/images.slider.js
@@ -0,0 +1,7 @@
+javascript
+document.querySelector('.slider').addEventListener('mouseover', function () {
+    this.style.animationPlayState = 'paused';
+});
+
+document.querySelector('.slider').addEventListener('mouseout', function () {
+    this.style.animationPlayState = 'running';
