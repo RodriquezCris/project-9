aed7917a17da065c2dc0690493d715269f37765a
RodriquezCris <142678152+RodriquezCris@users.noreply.github.com>
2023-10-02 20:45:38 -0500

    Rodriquez cris patch 1 (#2)
    * Create README.md
    
    * Add files via upload
    
    * initial commit
    
    * push to

--- Project 2/javascript/main.js
+++ Project 2/javascript/main.js
@@ -1,13 +1,18 @@
-const header = document.querySelector("header"),
-nav = document.querySelector("nav"),
-modeToggle = document.querySelector(".dark-light"),
-searchToggle = document.querySelector(".searchBox");
+const body = document.querySelector("body");
+const modeToggle = document.getElementById("mode-toggle");
+const modeStatus = document.querySelector(".mode-status");
 
-modeToggle.addEventListener("click", () => {
-modeToggle.classList.Toggle("active");
-header.classList.Toggle("dark");
-});
+function toggleMode() {
+  body.classList.toggle("dark-mode");
 
+  const modeMessage = body.classList.contains("dark-mode")
+    ? "Dark Mode"
+    : "Light Mode";
+
+  modeStatus.innerText = "Currently in " + modeMessage;
+}
+
+modeToggle.addEventListener("click", toggleMode);
 
 
 
---
+++ Project 2/javascript/mini-project/video-slider.js
@@ -0,0 +1,6 @@
+javascript;
+document.querySelector(".slider").addEventListener("mouseover", function () {
+  this.style.animationPlayState = "paused";
+});
+
+document.querySelector(".slider").addEventListener;
